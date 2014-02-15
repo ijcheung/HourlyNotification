bb83c943884e6b083086bb03c162812836bdc77a
Ivan Cheung <ivan.j.cheung@gmail.com>
2014-02-14 19:42:14 -0600

    Cleanup

--- src/com/espersoftworks/android/app/hourlynotification/MainActivity.java
+++ src/com/espersoftworks/android/app/hourlynotification/MainActivity.java
@@ -9,19 +9,5 @@ public class MainActivity extends FragmentActivity {
 		super.onCreate(savedInstanceState);
 		
 		setContentView(R.layout.activity_main);
-		
-//		getSupportFragmentManager().beginTransaction()
-//				.replace(android.R.id.content, new SettingsFragment())
-//				.commit();
-		
-//		AdView adView = new AdView(this, AdSize.BANNER, "ca-app-pub-2078905918254410/9975500487");
-//
-//		setListFooter(adView);
-//
-//		// Initiate a generic request to load it with an ad
-//		AdRequest request = new AdRequest();
-//		request.addTestDevice(AdRequest.TEST_EMULATOR);
-//		request.addTestDevice("630709098A8C59B1FCD8D4266AFDCC6B");
-//		adView.loadAd(request); 
 	}
 }
